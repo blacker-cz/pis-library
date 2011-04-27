0b9d4e982b5d2f0ba6537ae714e82dfa1a952e86
Lukáš Černý <cerny.l@gmail.com>
2011-04-27 12:10:19 +0200

    Fixed semicolon in schema update

--- db/update_2011_04_27.sql
+++ db/update_2011_04_27.sql
@@ -1,4 +1,4 @@
 -- Nullable collumns
 ALTER TABLE `publisher` CHANGE `address` `address` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_czech_ci NULL ;
 
-ALTER TABLE `user` CHANGE `address` `address` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_czech_ci NULL 
+ALTER TABLE `user` CHANGE `address` `address` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_czech_ci NULL ;
