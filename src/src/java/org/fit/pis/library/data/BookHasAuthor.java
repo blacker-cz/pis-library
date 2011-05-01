44bed894317c53ed63c0b44afcaf6d0e661419ed
unknown <quadrax@.(none)>
2011-05-01 10:45:44 +0200

    aaa

---
+++ src/src/java/org/fit/pis/library/data/BookHasAuthor.java
@@ -0,0 +1,119 @@
+/*
+ */
+
+package org.fit.pis.library.data;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlIDREF;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import org.fit.pis.library.back.converters.IntegerAdapter;
+
+/**
+ *
+ * @author Lukáš Černý <cerny.l@gmail.com>
+ */
+@Entity
+@Table(name = "author")
+@NamedQueries({
+	@NamedQuery(name = "Author.findAll", query = "SELECT a FROM Author a"),
+	@NamedQuery(name = "Author.findByIdauthor", query = "SELECT a FROM Author a WHERE a.idauthor = :idauthor"),
+	@NamedQuery(name = "Author.findByName", query = "SELECT a FROM Author a WHERE a.name = :name")})
+public class Author implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Basic(optional = false)
+//    @NotNull
+    @Column(name = "idauthor")
+	@XmlID
+	@XmlJavaTypeAdapter(IntegerAdapter.class)
+	private Integer idauthor;
+	@Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "name")
+	private String name;
+	@JoinTable(name = "book_has_author", joinColumns = {
+    	@JoinColumn(name = "author_idauthor", referencedColumnName = "idauthor")}, inverseJoinColumns = {
+    	@JoinColumn(name = "book_idbook", referencedColumnName = "idbook")})
+    @ManyToMany
+	@XmlIDREF
+	private Collection<Book> booksCollection;
+
+	public Author() {
+	}
+
+	public Author(Integer idauthor) {
+		this.idauthor = idauthor;
+	}
+
+	public Author(Integer idauthor, String name) {
+		this.idauthor = idauthor;
+		this.name = name;
+	}
+
+	public Integer getIdauthor() {
+		return idauthor;
+	}
+
+	public void setIdauthor(Integer idauthor) {
+		this.idauthor = idauthor;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Collection<Book> getBooksCollection() {
+		return booksCollection;
+	}
+
+	public void setBooksCollection(Collection<Book> booksCollection) {
+		this.booksCollection = booksCollection;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (idauthor != null ? idauthor.hashCode() : 0);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the id fields are not set
+		if (!(object instanceof Author)) {
+			return false;
+		}
+		Author other = (Author) object;
+		if ((this.idauthor == null && other.idauthor != null) || (this.idauthor != null && !this.idauthor.equals(other.idauthor))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		//return "org.fit.pis.library.data.Author[ idauthor=" + idauthor + " ]";
+                return name;
+	}
+
+}
---
+++ src/src/java/org/fit/pis/library/data/BookHasAuthorManager.java
@@ -0,0 +1,103 @@
+/**
+ * Stateless bean pro práci s daty knihy
+ */
+
+package org.fit.pis.library.data;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Vojtěch Sysel <xsysel03@setud.fit.vutbr.cz>
+ */
+@Stateless
+public class BookManager {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Book b)
+    {
+    	em.merge(b);
+    }
+	
+    public void remove(Book b)
+    {
+    	em.remove(em.merge(b));
+    }
+    
+	public void flush()
+	{
+		em.flush();
+	}
+    
+    @SuppressWarnings("unchecked")
+    public List<Book> findAll()
+    {
+    	return em.createQuery("SELECT b FROM Book b ORDER BY b.name ASC, b.year ASC").getResultList();
+    }
+	
+	/**
+	 * Find book by id
+	 * @param id
+	 * @return Book or null
+	 */
+	public Book findByIdbook(Integer id) {
+		em.flush();
+		
+		try {
+			Query query = em.createNamedQuery("Book.findByIdbook");
+			query.setParameter("idbook", id);
+			return (Book) query.getSingleResult();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	/**
+	 * Filter user list by some parameters
+	 * @param permitNumber
+	 * @param forename
+	 * @param surname
+	 * @param email
+	 * @param level
+	 * @return 
+	 */
+	public List<Book> find(String name, String author, Date yearFrom, Date yearTo, Genre genre, String isbn_issn) {
+		// genre SQL
+		String genreSQL = "";
+		if (genre != null) {
+			genreSQL = " AND b.genre.idgenre = :idgenreFilter ";
+		}
+		
+		Query query = em.createQuery(
+				"SELECT DISTINCT b FROM Book b "
+//				+ "JOIN b.Author a "
+				+ "WHERE "
+				+	"b.name LIKE :name AND "
+				+	"b.year BETWEEN :yearFrom AND :yearTo AND "
+				+	"b.code LIKE :isbn_issn "
+//				+	"AND a.name LIKE :author "
+				+ genreSQL 
+				+ "ORDER BY b.name ASC, b.year ASC"
+				);
+		//Query query = em.createQuery("SELECT b FROM Book b WHERE b.name LIKE :name AND b.authors.name LIKE :authorname" + genreSQL);
+		query.setParameter("name", "%" + name + "%");
+//		query.setParameter("author", "%" + author + "%");
+	
+		query.setParameter("isbn_issn", "%" + isbn_issn + "%");
+		// genre
+		if (genre != null) {
+			query.setParameter("idgenreFilter", genre.getIdgenre());
+		}
+		query.setParameter("yearFrom", yearFrom);
+		query.setParameter("yearTo", yearTo);
+		return (List<Book>) query.getResultList();
+	}
+}
